dceb72ea5f79fc938e66c20b133c3ca850f8f020
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2012-03-29 21:35:05 -0700

    v0.2.2

--- lib/fluent/plugin/out_librato_metrics.rb
+++ lib/fluent/plugin/out_librato_metrics.rb
@@ -1,7 +1,7 @@
 #
 # fluent-plugin-librato-metrics
 #
-# Copyright (C) 2011 FURUHASHI Sadayuki
+# Copyright (C) 2011-2012 Sadayuki Furuhashi
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
